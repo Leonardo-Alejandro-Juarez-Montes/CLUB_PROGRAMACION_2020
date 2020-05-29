d36e5df75795fb851beddab7e4134682af7777d7
Leonardo-Alejandro-Juarez-Montes <57674865+Leonardo-Alejandro-Juarez-Montes@users.noreply.github.com>
2020-05-29 17:23:16 -0500

    Delete Ejercicio_1.py

--- Ejercicio_1.py
+++
@@ -1,8 +0,0 @@
-print("Ejercicio Numero 1")
-print("Ingresa 2 números, de la siguiente forma")
-print("A B")
-cadena=input().split()
-A=int(cadena[0])
-B=int(cadena[1])
-suma=A+B
-print("La suma de ambos números es: "+str(suma)) 
